2990f21d69a08745a2bc298bbbb213e502c4104f
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2023-11-16 00:57:40 +0900

    pwa化

--- sample/app/layout.tsx
+++ sample/app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#b8e986" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
